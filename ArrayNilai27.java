6ea68265c37d03660db724dcbf728ed27379c1bd
Stridesti <tridestisasmita17@gmail.com>
2024-11-07 09:47:37 +0700

    Percobaan2

--- ArrayNilai27.java
+++ ArrayNilai27.java
@@ -3,15 +3,20 @@ import java.util.Scanner;
 public class ArrayNilai27 {
     public static void main(String[] args) {
         int[] nilaiAkhir = new int[10];
+        int jumlahMahasiswa = 10;
         Scanner scanner = new Scanner(System.in);
     
-    for (int i = 0; i< 10; i++) {
-        System.out.println("Masukkan akhir nilai ke-" + (i + 1) +": ");
+    for (int i = 0; i< nilaiAkhir.length; i++) {
+        System.out.println("Masukkan nilai akhir ke-" + (i + 0) +" : ");
         nilaiAkhir[i] = scanner.nextInt();
     }
-        System.out.println("Nilai yang dimasukkan:");
-    for (int i = 0; i< 10; i++) {
-        System.out.println("Nilai ke-" + (i + 1) + ": "+nilaiAkhir[i]);
+        System.out.println("\nDaftar Mahasiswa yang lulus:");
+    for (int i = 0; i< nilaiAkhir.length; i++) {
+        if (nilaiAkhir[i] > 70) {
+            System.out.println("Mahasiswa ke-" + (i + 0) + " lulus dengan nilai: " + nilaiAkhir[i]);
+        } else {
+            System.out.println("Mahasiswa ke-" + (i + 0) + " tidak lulus dengan nilai: " + nilaiAkhir[i]);
+        }
     }
     scanner.close();
     }
